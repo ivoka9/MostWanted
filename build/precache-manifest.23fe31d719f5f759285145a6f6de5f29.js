76962411a5dd24acb349288b4b34243e1b9d1773
ivoka <ivailo96g@gmail.com>
2020-09-23 00:06:35 -0400

    rdy

---
+++ build/precache-manifest.23fe31d719f5f759285145a6f6de5f29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea54cfb013d0f6f88c0de48340ee3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c69368df82ad9e7fb06",
+    "url": "/static/css/main.5fc2d0e6.chunk.css"
+  },
+  {
+    "revision": "d8c106f4b66269207a64",
+    "url": "/static/js/2.595c4f39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.595c4f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c69368df82ad9e7fb06",
+    "url": "/static/js/main.94dd22fd.chunk.js"
+  },
+  {
+    "revision": "396bf49f818fb456cb6f",
+    "url": "/static/js/runtime-main.fc52ae77.js"
+  },
+  {
+    "revision": "c93688bdf43ee0cee85bdf46536ab928",
+    "url": "/static/media/wanted_poster.c93688bd.jpg"
+  }
+]);
